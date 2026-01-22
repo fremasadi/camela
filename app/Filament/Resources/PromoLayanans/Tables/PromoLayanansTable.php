b7740e0281d747179244deaed17170ed096f593f
fremasadi <fremasadii@gmail.com>
2026-01-22 08:38:39 +0700

    revisi1

--- app/Filament/Resources/PromoLayanans/Tables/PromoLayanansTable.php
+++ app/Filament/Resources/PromoLayanans/Tables/PromoLayanansTable.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\PromoLayanans\Tables;
 
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\DeleteAction;
 use Filament\Actions\EditAction;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
