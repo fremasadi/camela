698157902ae9e0914af5c5b9ce11383d6d458751
fremasadi <fremasadii@gmail.com>
2026-04-08 11:17:11 +0700

    feat:summary in list booking

--- app/Filament/Resources/Bookings/Pages/ListBookings.php
+++ app/Filament/Resources/Bookings/Pages/ListBookings.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\Bookings\Pages;
 
 use App\Filament\Resources\Bookings\BookingResource;
+use App\Filament\Widgets\BookingStatsWidget;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
@@ -14,6 +15,13 @@ class ListBookings extends ListRecords
     {
         return [
             // CreateAction::make(),
+        ];
+    }
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            BookingStatsWidget::class,
         ];
     }
 }
